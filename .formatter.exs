8c05c503773979f8c027a056c5dd89f6eccaf938
Benjamin Danklin <mark@marks-engineering.com>
2021-09-04 08:03:25 +1000

    fix formatter for ecto

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,10 @@
 [
-  inputs: ["mix.exs", "{config,examples,lib,test}/**/*.{ex,exs}"],
+  import_deps: [:ecto],
+  inputs: [
+    "*.{ex,exs}",
+    "{config,lib,test}/**/*.{ex,exs}",
+    "apps/**/{config,lib,test,repo}/**/*.{ex,exs}"
+  ],
   locals_without_parens: [
     field: :*,
     belongs_to: :*,
@@ -7,6 +12,6 @@
     has_many: :*,
     many_to_many: :*,
     embeds_one: :*,
-    embeds_many: :*,
-
+    embeds_many: :*
+  ]
 ]

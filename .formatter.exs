697e3cf924ded2eeba7a417548d343c60846977c
Benjamin Danklin <mark@marks-engineering.com>
2021-09-04 07:10:00 +1000

    support ecto formatting

--- .formatter.exs
+++ .formatter.exs
@@ -1,3 +1,12 @@
 [
-  inputs: ["mix.exs", "{config,examples,lib,test}/**/*.{ex,exs}"]
+  inputs: ["mix.exs", "{config,examples,lib,test}/**/*.{ex,exs}"],
+  locals_without_parens: [
+    field: :*,
+    belongs_to: :*,
+    has_one: :*,
+    has_many: :*,
+    many_to_many: :*,
+    embeds_one: :*,
+    embeds_many: :*,
+
 ]

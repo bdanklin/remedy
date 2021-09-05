286eb2ca1969de71ec03330fc62b200f983629b4
Benjamin Danklin <mark@marks-engineering.com>
2021-09-06 00:36:34 +1000

    reaction schema

---
+++ lib/remedy/schema/reaction.ex
@@ -0,0 +1,11 @@
+defmodule Remedy.Schema.Reaction do
+  @moduledoc false
+  use Remedy.Schema, :model
+  @primary_key false
+
+  embedded_schema do
+    field :count, :integer
+    field :me, :boolean
+    embeds_one :emoji, Emoji
+  end
+end

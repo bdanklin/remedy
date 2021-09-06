b1905a50887a79b2072de8692e15ce6332201b49
Benjamin Danklin <mark@marks-engineering.com>
2021-09-06 14:18:23 +1000

    add payload type

--- lib/remedy/schema.ex
+++ lib/remedy/schema.ex
@@ -1,62 +1,5 @@
 defmodule Remedy.Schema do
   @moduledoc false
-  defmacro __using__(:model) do
-    quote do
-      use Ecto.Schema
-      import Ecto.Changeset
-      unquote(schema_helpers())
-      @before_compile
-    end
-  end
-
-  defmacro __before_compile__(_env) do
-    quote do
-      alias __MODULE__
-
-      def new(params) do
-        params
-        |> changeset()
-        |> apply_changes()
-      end
-
-      def changeset(params), do: changeset(%__MODULE__{id: params.id}, params)
-
-      def changeset(nil, params), do: changeset(%__MODULE__{id: params.id}, params)
-
-      # seems very out of scope?
-      def changeset(__MODULE__, params) do
-        __MODULE__
-        |> Cache.get(params.id)
-        |> changeset(params)
-      end
-
-      def changeset(%__MODULE__{}, params) do
-        model
-        |> cast(params, castable())
-        |> validations()
-        |> cast_embeds()
-      end
-
-      def validations(model), do: model
-
-      defp cast_embeds(cast_model) do
-        Enum.reduce(__MODULE__.__schema__(:embeds), cast_model, &cast_embed(&1, &2))
-      end
-
-      defp castable do
-        __MODULE__.__schema__(:fields) -- __MODULE__.__schema__(:embeds)
-      end
-
-      defoverridable(changeset: 1, changeset: 2, validations: 1)
-    end
-  end
-
-  defmacro __using__(_) do
-    quote do
-      unquote(schema_helpers())
-    end
-  end
-
   defp schema_helpers do
     parent = __MODULE__
 
@@ -95,6 +38,103 @@ defmodule Remedy.Schema do
         Voice,
         Webhook
       }
+    end
+  end
+
+  defmacro __using__(:model) do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      unquote schema_helpers()
+      @before_compile Remedy.Schema.Model
+    end
+  end
+
+  defmacro __using__(:payload) do
+    quote do
+      use Ecto.Schema
+      import Ecto.Changeset
+      unquote schema_helpers()
+      @before_compile Remedy.Schema.Payload
+    end
+  end
+
+  defmacro __using__(_) do
+    quote do
+      unquote schema_helpers()
+    end
+  end
+end
+
+defmodule Remedy.Schema.Model do
+  @moduledoc false
+  defmacro __before_compile__(_env) do
+    quote do
+      alias __MODULE__
+
+      def new(params) do
+        params
+        |> changeset()
+        |> apply_changes()
+      end
+
+      def changeset(params), do: changeset(%__MODULE__{}, params)
+
+      def changeset(nil, params), do: changeset(%__MODULE__{}, params)
+
+      def changeset(%__MODULE__{} = model, params) do
+        model
+        |> cast(params, castable())
+        |> cast_embeds()
+      end
+
+      defp cast_embeds(cast_model) do
+        Enum.reduce(__MODULE__.__schema__(:embeds), cast_model, &cast_embed(&1, &2))
+      end
+
+      defp castable do
+        __MODULE__.__schema__(:fields) -- __MODULE__.__schema__(:embeds)
+      end
+    end
+  end
+end
+
+defmodule Remedy.Schema.Payload do
+  @moduledoc false
+  defmacro __before_compile__(_env) do
+    quote do
+      alias __MODULE__
+
+      def new(params) do
+        params
+        |> changeset()
+        |> apply_changes()
+      end
+
+      def changeset(params), do: changeset(%__MODULE__{}, params)
+
+      def changeset(nil, params), do: changeset(%__MODULE__{}, params)
+
+      # seems very out of scope?
+      def changeset(__MODULE__, params) do
+        __MODULE__
+        |> Cache.get(params.id)
+        |> changeset(params)
+      end
+
+      def changeset(%__MODULE__{} = model, params) do
+        model
+        |> cast(params, castable())
+        |> cast_embeds()
+      end
+
+      defp cast_embeds(cast_model) do
+        Enum.reduce(__MODULE__.__schema__(:embeds), cast_model, &cast_embed(&1, &2))
+      end
+
+      defp castable do
+        __MODULE__.__schema__(:fields) -- __MODULE__.__schema__(:embeds)
+      end
     end
   end
 end

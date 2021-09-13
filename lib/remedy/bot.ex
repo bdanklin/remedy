75cb6e551f92bd097c42b188b9b671c6879942ad
Benjamin Danklin <mark@marks-engineering.com>
2021-09-13 14:12:29 +1000

    added full User name. not sure why this wont alias

--- lib/remedy/bot.ex
+++ lib/remedy/bot.ex
@@ -1,42 +1,29 @@
 defmodule Remedy.Bot do
-  defstruct [:hello]
+  import Remedy.ModelHelpers
 
   @moduledoc """
   Simple cache that stores information for the current user.
   """
 
-  use Agent
-  use Remedy.Schema, :models
-
-  @doc false
   def start_link(_args) do
     Agent.start_link(fn -> nil end, name: __MODULE__)
   end
 
-  @doc """
-  Returns the current user's state.
-  """
   @spec get() :: User.t() | nil
   def get do
     Agent.get(__MODULE__, fn user -> user end)
   end
 
-  @doc false
-  @spec put(User.t()) :: :ok
-  def put(%User{} = user) do
+  def put(%Remedy.Schema.User{} = user) do
     Agent.update(__MODULE__, fn _ -> user end)
   end
 
-  @doc false
-  @spec update(map) :: :ok
   def update(%{} = values) do
     Agent.update(__MODULE__, fn state ->
       struct(state, values)
     end)
   end
 
-  @doc false
-  @spec delete() :: :ok
   def delete do
     Agent.update(__MODULE__, fn _ -> nil end)
   end

d99abbfa1330899cce165ee1e87b5e6bb9e1b3ce
Benjamin Danklin <mark@marks-engineering.com>
2021-09-13 14:12:08 +1000

    added typespec

--- lib/remedy/gateway/websocket.ex
+++ lib/remedy/gateway/websocket.ex
@@ -4,11 +4,47 @@ defmodule Remedy.Gateway.Websocket do
   """
   use Remedy.Schema
 
-  @type shard :: :integer
+  @type t :: %__MODULE__{
+          token: token,
+          shard: shard,
+          gateway: gateway,
+          session_id: session_id,
+          shard_pid: shard_pid,
+          heartbeat_interval: heartbeat_interval,
+          gun_worker: gun_worker,
+          gun_conn: gun_conn,
+          gun_data_stream: gun_data_stream,
+          zlib_context: zlib_context,
+          last_heartbeat_send: last_heartbeat_send,
+          last_heartbeat_ack: last_heartbeat_ack,
+          heartbeat_ack: heartbeat_ack,
+          heartbeat_timer: heartbeat_timer,
+          payload_op_code: payload_op_code,
+          payload_op_event: payload_op_event,
+          payload_sequence: payload_sequence,
+          payload_data: payload_data,
+          payload_dispatch_event: payload_dispatch_event
+        }
+
+  @type token :: String.t()
+  @type shard :: integer()
+  @type gateway :: String.t()
   @type session_id :: String.t()
   @type shard_pid :: pid()
   @type heartbeat_interval :: integer()
-  @type worker :: pid()
+  @type gun_worker :: pid()
+  @type gun_conn :: pid()
+  @type gun_data_stream :: Stream.t()
+  @type zlib_context :: term()
+  @type last_heartbeat_send :: DateTime.t()
+  @type last_heartbeat_ack :: DateTime.t()
+  @type heartbeat_ack :: boolean()
+  @type heartbeat_timer :: integer()
+  @type payload_op_code :: integer()
+  @type payload_op_event :: atom()
+  @type payload_sequence :: integer()
+  @type payload_data :: any()
+  @type payload_dispatch_event :: atom()
 
   @primary_key false
   embedded_schema do
@@ -16,30 +52,26 @@ defmodule Remedy.Gateway.Websocket do
     field :token, :string, redact: true, default: Application.get_env(:remedy, :token)
     field :shard, :integer
     field :gateway, :string
-
     # Need to store to maintain connection
     field :session_id, :string
     field :shard_pid, :any, virtual: true
     field :heartbeat_interval, :integer
-
-    # Gun INfo
-    field :gun_worker, :any, virtual: true
-    field :gun_conn, :any, virtual: true
-    field :gun_data_stream, :any, virtual: true
-    field :zlib_context, :any, virtual: true
-
     # Heartbeat
     field :last_heartbeat_send, :utc_datetime
     field :last_heartbeat_ack, :utc_datetime
     field :heartbeat_ack, :boolean, default: false
     field :heartbeat_timer, :any, virtual: true
-
+    # Gun INfo
+    field :gun_worker, :any, virtual: true
+    field :gun_conn, :any, virtual: true
+    field :gun_data_stream, :any, virtual: true
+    field :zlib_context, :any, virtual: true
     # Payload items that can actually be used.
     field :payload_op_code, :integer, default: 0
     field :payload_op_event, :string, default: ""
     field :payload_sequence, :integer
     field :payload_data, :any, virtual: true
-    field :payload_dispatch_event, :string
+    field :payload_dispatch_event, :any, virtual: true
   end
 
   @doc """

82f243f155bfed190ba674104cb2e2dcc57b181e
bdanklin <mark@marks-engineering.com>
2021-10-06 08:32:35 +1000

    separate session superv.

--- lib/remedy/gateway/session.ex
+++ lib/remedy/gateway/session.ex
@@ -99,21 +99,3 @@ defmodule Remedy.Gateway.Session do
     {:noreply, socket}
   end
 end
-
-defmodule Remedy.Gateway.SessionSupervisor do
-  @moduledoc false
-
-  use Supervisor
-
-  alias Remedy.Gateway.Session
-
-  def start_link(%{shard: shard} = opts) do
-    Supervisor.start_link(__MODULE__, opts, name: :"Shard-#{shard}")
-  end
-
-  def init(opts) do
-    children = [{Session, opts}]
-
-    Supervisor.init(children, strategy: :one_for_all, max_restarts: 3, max_seconds: 60)
-  end
-end

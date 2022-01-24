e04604da7f77b1065d50a536ab5b5f674ef6e441
bdanklin <mark@marks-engineering.com>
2022-01-24 15:06:03 +1000

    reorg files

---
+++ lib/remedy/voice.ex
@@ -0,0 +1,21 @@
+defmodule Remedy.Voice do
+  @moduledoc """
+  Voice entry point.
+  """
+  use Supervisor
+
+  alias Remedy.Voice.Pool
+
+  def start_link(args) do
+    Supervisor.start_link(__MODULE__, args, name: __MODULE__)
+  end
+
+  def init(_args) do
+    children = [
+      {Pool, []},
+      {Stagehand, []}
+    ]
+
+    Supervisor.init(children, strategy: :one_for_one)
+  end
+end

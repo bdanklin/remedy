53099cc44ebb1b8241e1c8f58415b837ff54958e
bdanklin <mark@marks-engineering.com>
2022-02-18 23:39:32 +1000

    refactor dispatch sup

--- lib/remedy/dispatch.ex
+++ lib/remedy/dispatch.ex
@@ -1,20 +1,31 @@
 defmodule Remedy.Dispatch do
-  @moduledoc false
-  alias Remedy.Dispatch.Pipeline
-  alias Remedy.Buffer
-  alias Remedy.Buffer.Out
-
+  @moduledoc """
+  The Dispatch module is concerned with processing events from `Remedy.Gateway` and `Remedy.Voice`.
+  """
   use Supervisor
 
+  # TODO: Functions for rearranging Broadway Topology
+
+  @doc false
   def start_link(_args) do
     Supervisor.start_link(__MODULE__, [], name: __MODULE__)
   end
 
+  @doc false
+  def child_spec(init_arg) do
+    default = %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [init_arg]},
+      type: :supervisor
+    }
+
+    Supervisor.child_spec(default, [])
+  end
+
+  @doc false
   def init(_args) do
     children = [
-      {Buffer, []},
-      {Pipeline, []},
-      {Buffer.Out, []}
+      {Remedy.Dispatch.Pipeline, []}
     ]
 
     Supervisor.init(children, strategy: :one_for_one, max_restarts: 1000, max_seconds: 60)

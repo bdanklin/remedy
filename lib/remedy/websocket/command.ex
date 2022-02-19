6cd251bd9ce696be5b56901b59db8dd7286711d9
bdanklin <mark@marks-engineering.com>
2022-02-19 23:17:48 +1000

    cleanup

--- lib/remedy/websocket/command.ex
+++ lib/remedy/websocket/command.ex
@@ -1,9 +1,13 @@
 defmodule Remedy.Websocket.Command do
   @moduledoc false
 
-  import Remedy.CaseHelpers, only: [to_pascal: 1]
+  import Remedy.CaseHelpers,
+    only: [to_pascal: 1]
 
-  def send(%{mod: mod} = socket, command, opts \\ []) do
+  import Remedy.CastHelpers,
+    only: [deep_blast: 1, deep_string_key: 1]
+
+  def send(%{mod: mod, conn: conn, data_stream: data_stream} = socket, command, opts \\ []) do
     op_code_mod =
       [Remedy, mod, OPCode]
       |> Module.concat()
@@ -21,19 +25,17 @@ defmodule Remedy.Websocket.Command do
 
     payload = module.send(socket, opts)
 
-    %{
-      "d" => payload,
-      "op" => op_code_mod.to_integer(command)
-    }
-    |> Remedy.CastHelpers.deep_struct_blaster()
-    |> Remedy.CastHelpers.deep_string_key()
-    |> :erlang.term_to_binary()
-    |> websocket_send(socket)
-  end
+    payload =
+      %{
+        "d" => payload,
+        "op" => op_code_mod.to_integer(command)
+      }
+      |> deep_blast()
+      |> deep_string_key()
+      |> :erlang.term_to_binary()
 
-  defp websocket_send(payload, %{conn: conn, data_stream: data_stream} = socket) do
-    case :gun.ws_send(conn, data_stream, {:binary, payload}) do
-      :ok -> socket
+    with :ok <- :gun.ws_send(conn, data_stream, {:binary, payload}) do
+      socket
     end
   end
 end
